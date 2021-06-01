ee34cff1dc63fb64c23a957bd48c9f87f52ef8a0
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-06-01 03:37:42 +0200

    Controller and Service for BBB API

--- src/main/kotlin/de/thm/mni/ii/classroom/controller/BBBApiController.kt
+++ src/main/kotlin/de/thm/mni/ii/classroom/controller/BBBApiController.kt
@@ -1,21 +1,44 @@
 package de.thm.mni.ii.classroom.controller
 
-import de.thm.mni.ii.classroom.model.dto.CreateRoomResponse
-import de.thm.mni.ii.classroom.model.dto.JoinRoomResponse
+import de.thm.mni.ii.classroom.exception.MissingMeetingIDException
+import de.thm.mni.ii.classroom.model.dto.*
 import de.thm.mni.ii.classroom.services.ClassroomService
+import org.slf4j.LoggerFactory
 import org.springframework.util.MimeTypeUtils
 import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
+import kotlin.Exception
 
 @RestController
 @RequestMapping("/api/v1", produces = [MimeTypeUtils.APPLICATION_XML_VALUE])
 class BBBApiController(private val classroomService: ClassroomService) {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @GetMapping("/create")
-    fun createClassroomInstance(@RequestParam param: MultiValueMap<String, String>): Mono<CreateRoomResponse> = classroomService.createClassroom(param)
+    fun createClassroomInstance(@RequestParam param: MultiValueMap<String, String>): Mono<ReturnCodeBBB> =
+        classroomService.createClassroom(param).onErrorResume {
+            errorHandler(it)
+        }
 
     @GetMapping("/join")
-    fun joinUserToClassroom(@RequestParam param: MultiValueMap<String, String>): Mono<JoinRoomResponse> = classroomService.joinClassroom(param)
+    fun joinUserToClassroom(@RequestParam param: MultiValueMap<String, String>): Mono<ReturnCodeBBB> =
+        classroomService.joinClassroom(param).onErrorResume {
+            errorHandler(it)
+        }
+
+    @GetMapping("/isMeetingRunning")
+    fun isMeetingRunning(@RequestParam param: MultiValueMap<String, String>): Mono<ReturnCodeBBB> = classroomService.isMeetingRunning(param)
+
+    private fun errorHandler(throwable: Throwable): Mono<MessageBBB> {
+        return Mono.create {
+            if (throwable is MissingMeetingIDException) {
+                it.success(MessageBBB(false, "missingParamMeetingID", "You must specify a meeting ID for the meeting."))
+            } else {
+                it.error(throwable)
+            }
+        }
+    }
 
 }
--- src/main/kotlin/de/thm/mni/ii/classroom/services/ClassroomService.kt
+++ src/main/kotlin/de/thm/mni/ii/classroom/services/ClassroomService.kt
@@ -1,7 +1,9 @@
 package de.thm.mni.ii.classroom.services
 
-import de.thm.mni.ii.classroom.model.dto.CreateRoomResponse
-import de.thm.mni.ii.classroom.model.dto.JoinRoomResponse
+import de.thm.mni.ii.classroom.exception.MissingMeetingIDException
+import de.thm.mni.ii.classroom.model.DigitalClassroomInstance
+import de.thm.mni.ii.classroom.model.dto.*
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.util.MultiValueMap
 import reactor.core.publisher.Mono
@@ -10,8 +12,34 @@ import reactor.kotlin.core.publisher.toMono
 @Component
 class ClassroomService {
 
-    fun createClassroom(param: MultiValueMap<String, String>): Mono<CreateRoomResponse> = CreateRoomResponse().toMono()
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
-    fun joinClassroom(param: MultiValueMap<String, String>): Mono<JoinRoomResponse> = JoinRoomResponse().toMono()
+    private val classrooms = HashMap<String, DigitalClassroomInstance>()
+
+    fun createClassroom(param: MultiValueMap<String, String>): Mono<ReturnCodeBBB> {
+        return Mono.create {
+            val meetingID = getMeetingID(param)
+            val classroomInstance = classrooms.computeIfAbsent(meetingID) { DigitalClassroomInstance(param) }
+            it.success(CreateRoomBBB(classroomInstance))
+        }
+    }
+
+    fun joinClassroom(param: MultiValueMap<String, String>): Mono<ReturnCodeBBB> = JoinRoomBBB().toMono()
+
+    fun isMeetingRunning(param: MultiValueMap<String, String>): Mono<ReturnCodeBBB> {
+        return Mono.create {
+            val meetingID = getMeetingID(param)
+            it.success(IsMeetingRunningBBB(classrooms.containsKey(meetingID)))
+        }
+    }
+
+    private fun getMeetingID(param: MultiValueMap<String, String>): String {
+        val meetingID = param.getFirst("meetingID")
+        if (meetingID.isNullOrEmpty()) {
+            throw MissingMeetingIDException()
+        }
+        logger.debug("meetingID: $meetingID")
+        return meetingID
+    }
 
 }
