345095f55cf4747358212228807798b33cd06c40
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-08-26 00:42:42 +0200

    cleanup

--- src/main/kotlin/de/thm/mni/ii/classroom/model/message/Message.kt
+++
@@ -1,9 +0,0 @@
-package de.thm.mni.ii.classroom.model.message
-
-import java.time.LocalDateTime
-
-data class Message(
-    val jwt: String,
-    val timestamp: LocalDateTime,
-    val message: String
-)
