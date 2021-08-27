ff6706dcba8cf31856e00e720f8e78309eb72ed1
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-08-27 16:39:14 +0200

    Backend Classroom API -> RSocket. Ticket init implemented. SetOnceProperty for "lateinit val" workaround. Tickets have ticketId from classroom exclusive sequence.

---
+++ src/main/kotlin/de/thm/mni/ii/classroom/util/SetOnce.kt
@@ -0,0 +1,28 @@
+package de.thm.mni.ii.classroom.util
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+inline fun <reified T> setOnce(): ReadWriteProperty<Any, T> = SetOnceProperty()
+
+class SetOnceProperty<T> : ReadWriteProperty<Any, T> {
+
+    private object EMPTY
+
+    private var value: Any? = EMPTY
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): T {
+        if (value == EMPTY) {
+            throw IllegalStateException("Value isn't initialized")
+        } else {
+            return value as T
+        }
+    }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+        if (this.value != EMPTY) {
+            throw IllegalStateException("Value is initialized")
+        }
+        this.value = value
+    }
+}
