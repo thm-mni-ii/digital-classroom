1c093cfeed2d2974ffaf2d7620d39c8120ee64fa
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-09-10 23:32:15 +0200

    fixed attendee display in getMeetings and getMeetingInfo (#39)

--- src/main/kotlin/de/thm/mni/ii/classroom/model/api/Attendees.kt
+++ src/main/kotlin/de/thm/mni/ii/classroom/model/api/Attendees.kt
@@ -1,22 +1,33 @@
 package de.thm.mni.ii.classroom.model.api
 
+import de.thm.mni.ii.classroom.model.classroom.DigitalClassroom
 import de.thm.mni.ii.classroom.model.classroom.User
 import javax.xml.bind.annotation.XmlElement
 import javax.xml.bind.annotation.XmlRootElement
 import javax.xml.bind.annotation.XmlType
 
-@XmlRootElement(name = "attendee")
-@XmlType(propOrder=[
-    "userID",
-    "fullName",
-    "role",
-    "isPresenter",
-    "isListeningOnly",
-    "hasJoinedVoice",
-    "hasVideo",
-    "clientType"
-])
-class Attendee(user: User?) {
+@XmlRootElement(name = "attendees")
+class Attendees(users: Set<User>? = null) {
+
+    @XmlElement(name = "attendee")
+    private val attendees = users?.map(::Attendee)
+
+}
+
+@XmlType(name = "attendee",
+    propOrder = [
+        "userID",
+        "fullName",
+        "role",
+        "isPresenter",
+        "isListeningOnly",
+        "hasJoinedVoice",
+        "hasVideo",
+        "clientType"
+    ]
+)
+
+class Attendee(user: User? = null) {
     @XmlElement private val userID: String? = user?.userId
     @XmlElement private val fullName: String? = user?.fullName
     @XmlElement private val role: String? = user?.userRole?.name
@@ -25,7 +36,4 @@ class Attendee(user: User?) {
     @XmlElement private val hasJoinedVoice = false
     @XmlElement private val hasVideo = false
     @XmlElement private val clientType = "CLASSROOM"
-
-    constructor(): this(null)
-
 }
