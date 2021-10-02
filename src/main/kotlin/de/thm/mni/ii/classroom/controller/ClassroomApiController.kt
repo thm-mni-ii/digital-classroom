f333af94f31257940ca48cbf4b8b73a6931dffb4
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-10-03 01:37:41 +0200

    JWTs expire and refresh.

--- src/main/kotlin/de/thm/mni/ii/classroom/controller/ClassroomApiController.kt
+++ src/main/kotlin/de/thm/mni/ii/classroom/controller/ClassroomApiController.kt
@@ -10,11 +10,13 @@ import de.thm.mni.ii.classroom.util.component2
 import org.apache.commons.lang3.RandomStringUtils
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
@@ -36,6 +38,7 @@ class ClassroomApiController(
      * @return
      */
     @GetMapping("/join")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun joinClassroom(auth: ClassroomAuthentication, originalExchange: ServerWebExchange): Mono<ServerHttpResponse> {
         return Mono.just(generateRefreshToken(auth.principal))
             .map { refreshToken ->
@@ -49,6 +52,7 @@ class ClassroomApiController(
     }
 
     @GetMapping("/refresh")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun refreshToken(
         auth: ClassroomAuthentication,
         originalExchange: ServerWebExchange,
@@ -56,10 +60,17 @@ class ClassroomApiController(
     ): Mono<ServerHttpResponse> {
         return classroomTokenRepository
             .findRefreshToken(refreshToken)
+            .switchIfEmpty {
+                val ex = UnauthorizedException("Invalid refresh token provided by user ${auth.user?.fullName}")
+                logger.error("", ex)
+                Mono.error(ex)
+            }
             .filter { user ->
-                user === auth.user
+                user == auth.user
             }.switchIfEmpty {
-                Mono.error(UnauthorizedException("Invalid refresh token provided by user ${auth.user?.fullName}"))
+                val ex = UnauthorizedException("Owner of refresh token does not match requester!")
+                logger.error("", ex)
+                Mono.error(ex)
             }.map { user ->
                 val newRefreshToken = generateRefreshToken(user)
                 Pair(user, setHeader("refresh_token", newRefreshToken, originalExchange))
--- src/main/kotlin/de/thm/mni/ii/classroom/security/jwt/ClassroomJwtService.kt
+++ src/main/kotlin/de/thm/mni/ii/classroom/security/jwt/ClassroomJwtService.kt
@@ -2,23 +2,35 @@ package de.thm.mni.ii.classroom.security.jwt
 
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.JWSHeader
-import com.nimbusds.jose.JWSObject
-import com.nimbusds.jose.Payload
 import com.nimbusds.jose.crypto.MACSigner
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
 import de.thm.mni.ii.classroom.model.classroom.User
+import de.thm.mni.ii.classroom.properties.ClassroomProperties
 import de.thm.mni.ii.classroom.properties.JwtProperties
 import de.thm.mni.ii.classroom.util.repeatLength
-import net.minidev.json.JSONObject
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
+import java.time.Instant
+import java.util.Date
 import javax.crypto.spec.SecretKeySpec
 
 @Component
-class ClassroomJwtService(jwtProperties: JwtProperties) : ReactiveJwtDecoder {
+class ClassroomJwtService(
+    private val jwtProperties: JwtProperties,
+    private val classroomProperties: ClassroomProperties
+) : ReactiveJwtDecoder {
+
+    private fun JWTClaimsSet.Builder.addClaims(claims: Map<String, Any>): JWTClaimsSet.Builder {
+        claims.entries.forEach { (key, value) ->
+            this.claim(key, value)
+        }
+        return this
+    }
 
     private val secretKey = SecretKeySpec(jwtProperties.secret.repeatLength(60).toByteArray(), "HmacSHA384")
     private val reactiveJwtDecoder = NimbusReactiveJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS384).build()
@@ -26,9 +38,15 @@ class ClassroomJwtService(jwtProperties: JwtProperties) : ReactiveJwtDecoder {
 
     fun createToken(user: User): Mono<String> {
         return Mono.defer {
-            val jwsObject = JWSObject(JWSHeader(JWSAlgorithm.HS384), Payload(JSONObject(user.getJwtClaims())))
-            jwsObject.sign(jwsSigner)
-            Mono.just(jwsObject.serialize())
+            val claimsSet = JWTClaimsSet.Builder()
+                .subject(jwtProperties.jwtSubject)
+                .issuer(classroomProperties.host)
+                .expirationTime(Date(Date().time + jwtProperties.expiration * 1000))
+                .addClaims(user.getJwtClaims())
+                .build()
+            val signedJwt = SignedJWT(JWSHeader(JWSAlgorithm.HS384), claimsSet)
+            signedJwt.sign(jwsSigner)
+            Mono.just(signedJwt.serialize())
         }
     }
 
