3f8703e0fb17623f758048a7acc9c21bd566b8ee
Max Stephan <max.stephan@mni.thm.de>
2021-10-28 16:45:30 +0200

    [time-formatter] use date-fns functions

--- web-gui/src/app/util/time-formatter.service.ts
+++ web-gui/src/app/util/time-formatter.service.ts
@@ -1,85 +1,50 @@
-/**
- * Quelle: https://muffinman.io/blog/javascript-time-ago-function/
- */
-
 import { Injectable } from '@angular/core';
+import { formatDistanceToNowStrict, formatRelative } from 'date-fns';
+import { de } from 'date-fns/locale';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TimeFormatterService {
+  private formatDistanceLocale: any = {
+    xSeconds: { one: 'gerade eben', other: 'gerade eben' },
+    xMinutes: { one: 'vor {{count}} Minute', other: 'vor {{count}} Minuten' },
+  };
 
-  constructor() { }
+  private formatRelativeLocal: any = {
+    today: "'heute um' p",
+    yesterday: "'gestern um' p",
+    lastWeek: "dd. MMM 'um' p",
+    other: "dd. MMM 'um' p",
+  };
 
-  private months: string[] = [
-    'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
-    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
-  ];
+  private formatDistance = (token: any, count: number) => {
+    if (!Object.keys(this.formatDistanceLocale).includes(token)) return '';
 
-  // @ts-ignore
-  private getFormattedDate(date, prefomattedDate: string = undefined, hideYear = false) {
-    const day = date.getDate();
-    const month = this.months[date.getMonth()];
-    const year = date.getFullYear();
-    const hours = date.getHours();
-    let minutes = date.getMinutes();
+    const res = this.formatDistanceLocale[token as any];
 
-    if (minutes < 10) {
-      // Adding leading zero to minutes
-      minutes = `0${ minutes }`;
+    if (count === 1) {
+      return res.one.replace('{{count}}', count);
     }
 
-    if (prefomattedDate) {
-      // Today at 10:20
-      // Yesterday at 10:20
-      return `${ prefomattedDate } um ${ hours }:${ minutes }`;
-    }
+    return res.other.replace('{{count}}', count);
+  };
 
-    if (hideYear) {
-      // 10. January at 10:20
-      return `${ day }. ${ month } um ${ hours }:${ minutes }`;
-    }
+  private locale = {
+    ...de,
+    formatRelative: (token: any) => this.formatRelativeLocal[token],
+    formatDistance: this.formatDistance,
+  };
 
-    // 10. January 2017. at 10:20
-    return `${ day }. ${ month } ${ year }. um ${ hours }:${ minutes }`;
-  }
+  public timeAgo(dateParam: number): string {
+    const date = new Date(dateParam);
 
+    const timeAgo = formatDistanceToNowStrict(date, {
+      locale: { ...de, formatDistance: this.formatDistance },
+    });
 
-// --- Main function
-// @ts-ignore
-  public timeAgo(dateParam): string {
-    if (!dateParam) {
-      // @ts-ignore
-      return null;
-    }
+    if (timeAgo) return timeAgo;
 
-    const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
-    const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
-    const today: number = Date.now()
-    const yesterday = new Date(today - DAY_IN_MS);
-    const seconds = Math.round((today - date) / 1000);
-    const minutes = Math.round(seconds / 60);
-    const isToday = new Date(today).toDateString() === date.toDateString();
-    const isYesterday = yesterday.toDateString() === date.toDateString();
-    const isThisYear = new Date(today).getFullYear() === date.getFullYear();
-
-
-    if (seconds < 30) {
-      return 'gerade eben';
-    //} else if (seconds < 30) {
-    // return `vor ${ seconds } Sek.`;
-    } else if (seconds < 90) {
-      return 'vor 1 Minute';
-    } else if (minutes < 60) {
-      return `vor ${ minutes } Minuten`;
-    } else if (isToday) {
-      return this.getFormattedDate(date, 'Heute'); // Today at 10:20
-    } else if (isYesterday) {
-      return this.getFormattedDate(date, 'Gestern'); // Yesterday at 10:20
-    } else if (isThisYear) {
-      return this.getFormattedDate(date, undefined, true); // 10. January at 10:20
-    }
-
-    return this.getFormattedDate(date); // 10. January 2017. at 10:20
+    return formatRelative(date, new Date(), { locale: this.locale });
   }
 }
