21ca214422f14257c9815104b5f60a0ae777f1c0
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2021-11-25 21:11:04 +0100

    Fixed positional problems.

--- web-gui/src/app/page-components/classroom/conference-list/conference-list.component.ts
+++ web-gui/src/app/page-components/classroom/conference-list/conference-list.component.ts
@@ -23,16 +23,22 @@ export class ConferenceListComponent implements OnInit {
       currentUser => this.currentUser = currentUser
     )
     this.classroomService.conferencesObservable.subscribe(
-      conferences => {
-        this.plenaryConference = conferences.find(conf => this.isPlenaryConference(conf))
-        if (this.plenaryConference !== undefined) {
-          this.conferences = conferences.filter(conf => !this.isPlenaryConference(conf))
-        } else this.conferences = conferences
-      }
+      conferences => this.setConferences(conferences)
+    )
+    this.classroomService.classroomInfoObservable.subscribe(
+      _ => this.setConferences(this.conferences!!)
     )
   }
 
-  private isPlenaryConference(conference: ConferenceInfo) {
-    return conference.conferenceId === this.classroomService.classroomInfo?.plenaryConference
+  public isPlenaryConference(conference: ConferenceInfo): boolean {
+    return conference.conferenceId === this.classroomService.classroomInfo!!.plenaryConference
+  }
+
+  private setConferences(conferences: ConferenceInfo[]) {
+    this.plenaryConference = conferences.find(conf => this.isPlenaryConference(conf))
+    if (this.plenaryConference !== undefined) {
+      this.conferences = conferences.filter(conf => !this.isPlenaryConference(conf))
+      this.conferences.unshift(this.plenaryConference)
+    } else this.conferences = conferences
   }
 }
